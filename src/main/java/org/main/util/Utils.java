03f77fb1a5a2b9b699f1f47df32502de7d4e24b2
Ranjit Gouda <ranjitgouda013@gmail.com>
2023-03-22 22:53:27 +0530

    Deleted the Unused Method

--- src/main/java/org/main/util/Utils.java
+++ src/main/java/org/main/util/Utils.java
@@ -1,16 +1,9 @@
 package org.main.util;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.main.model.Link;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,24 +11,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
-
-    public static void readExcel(String filePath) {
-        File file = new File(filePath);
-        try {
-            FileInputStream inputStream = new FileInputStream(file);
-            Workbook baeuldungWorkBook = new XSSFWorkbook(inputStream);
-            for (Sheet sheet : baeuldungWorkBook) {
-                int firstRow = sheet.getFirstRowNum();
-                int lastRow = sheet.getLastRowNum();
-                for (int index = firstRow + 1; index <= lastRow; index++) {
-                    Row row = sheet.getRow(index);
-                }
-            }
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static List<Link> readCsv(Path filePath) {
         try {
